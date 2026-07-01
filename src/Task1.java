510e3728b1079e197d34814cc16dfc1fe61190c1
sreissig-git <reissisn@mail.uc.edu>
2026-07-01 03:05:33 -0400

    Task 1 Complete

---
+++ src/Task1.java
@@ -0,0 +1,24 @@
+//Class Task1
+//  main()
+//      num start = 0
+//      num end = 30
+//      num i = 0
+//      for (i = start; i <= end; i = i + 1)
+//          output i
+//      endfor
+//  return
+//EndClass
+public class Task1 {
+    public static void main(String[] args)
+    {
+        // Count from 0 to 30 using a for-loop
+        int start = 0;
+        int end = 30;
+        int i = 0;
+
+        for (i = start; i <= end; i = i + 1)
+        {
+            System.out.print(i + " ");
+        }
+    }
+}
