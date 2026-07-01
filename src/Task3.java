7a7c4ad566df3e7e840844519dd11677e4aee55f
sreissig-git <reissisn@mail.uc.edu>
2026-07-01 03:10:35 -0400

    Task 3 Complete

--- src/Task3.java
+++ src/Task3.java
@@ -8,14 +8,17 @@
 //      endfor
 //  return
 //EndClass
-public class Task3 {
-	public static void main(String[] args) {
+public class Task3
+{
+	public static void main(String[] args)
+    {
 		// Count up from 0 to 18 by 3 using a for-loop
 		double start = 0.0;
 		double end = 18.0;
 		double i = start;
 
-		for (i = start; i <= end; i = i + 3) {
+		for (i = start; i <= end; i = i + 3)
+        {
 			System.out.print((int) i + " ");
 		}
 	}
